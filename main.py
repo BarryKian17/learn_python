15df97673f8d82cfd56b41eefcf0eceec22a0003
kaung set linn <kaungsetisking@gmail.com>
2023-10-30 10:53:27 +0630

    py-lesson 5

---
+++ main.py
@@ -0,0 +1,15 @@
+# get name
+# name = input("What is your name?" )
+# print(f"Hello {name}")
+
+# print(input('Name:' ))
+
+# age = input('What is your age ?')
+
+# print(f"Hello {age} years old {name}")
+
+#area calculation
+p = 3.14
+r = int(input('r :'))
+area = p*r**2
+print(area)
