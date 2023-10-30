cc59bb2eeb92416855caf656718b1ed80e692318
kaung set linn <kaungsetisking@gmail.com>
2023-10-30 13:56:19 +0630

    py-lesson 10

--- loops.py
+++ loops.py
@@ -5,3 +5,4 @@ for n in names:
         break
     else:
         print(n)
+
---
+++ while_loop.py
@@ -0,0 +1,10 @@
+# num = int(input('number : '))
+# while 0 < num:
+#     print("hello")
+#     num -= 1
+
+num = int(input('number : '))
+while 0 < num:
+    if num % 2 == 0:
+        print(f'{num} is even')
+    num-=1
