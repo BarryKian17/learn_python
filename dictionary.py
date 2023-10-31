ccf642edc31838d5806d6fd2c76b2120eec459bd
kaung set linn <kaungsetisking@gmail.com>
2023-10-31 13:56:34 +0630

    py-lesson 14

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+person = {
+    'name':'barry',
+    'age':'20'
+}
+# person['address'] = 'bagan'
+
+# if 'a' in person:
+#     print("name is here")
+# else:
+#     print("ok pepsi")
+
+person_values = list(person.values())
+print(person_values)
