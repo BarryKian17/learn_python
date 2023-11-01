3a9100ae4f58ded3db263a4b335683969695a5ca
kaung set linn <kaungsetisking@gmail.com>
2023-11-01 10:00:50 +0630

    py-lesson 16

--- example.py
+++ example.py
@@ -9,6 +9,9 @@ while True:
         continue
     else:
         break
-
-for (key,value) in person.items():
-    print(f"{key} is {value} years old")
+ages = list(person.values())
+for age in set(ages):
+    count = ages.count(age)
+    print(f"{age} years old - {count}")
+# for (key,value) in person.items():
+#     print(f"{key} is {value} years old")
---
+++ sorted_set.py
@@ -0,0 +1,4 @@
+nums = [2,5,56,48,6,1,3,5,2,5,7,6]
+
+for num in set(nums):
+    print(num)
