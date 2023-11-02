6c819740d9bf6e67c94870940958d7712185cccc
kaung set linn <kaungsetisking@gmail.com>
2023-11-02 11:22:51 +0630

    py-lesson 24

---
+++ lambdas.py
@@ -0,0 +1,10 @@
+# def add(n1,n2):
+#     return n1+n2
+
+# add = lambda n1,n2:n1+n2
+# print(add(10,2))
+
+nums = [1,2,3,4,5,6,7,8,9,10,12,13,16,18]
+
+
+print(list(filter(lambda num:num%2 ==0,nums)))
